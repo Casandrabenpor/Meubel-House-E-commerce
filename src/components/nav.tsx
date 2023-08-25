3cc33886074196c36c1a5ce81e372d0a3c745f89
casandrabenpor <casandra211@gmail.com>
2023-08-25 14:25:18 +0200

    Logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import { TopNav } from '../styled/navStyled';
+import Logo from '../assets/home/Logo.png';
 export const Nav = () => {
   return (
     <TopNav>
+      <div>
+        <img src={Logo} alt="Logo" width={50} height={50} />
+      </div>
       <h3>Meubel House</h3>
       <ul>
         <li>
