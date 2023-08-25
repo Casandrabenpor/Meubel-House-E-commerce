a8c3b1a2f819238b9dc9f4d84719a810a2758b8b
casandrabenpor <casandra211@gmail.com>
2023-08-25 14:53:27 +0200

    nav

--- src/styled/navStyled.tsx
+++ src/styled/navStyled.tsx
@@ -1,8 +1,38 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { colors } from './theme';
 
 export const TopNav = styled.div`
   display: flex;
   flex-direction: row;
-  width: 20%;
+  justify-content: space-between;
   padding: 2em;
+`;
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1em;
+`;
+export const CustomList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  justify-content: space-between;
+  gap: 3em;
+`;
+export const CustomLink = styled(Link)`
+  text-decoration: none;
+  color: ${colors.black};
+  font-family: 'Poppins';
+
+  &:hover {
+    border-bottom: 2px solid #b88e2f;
+  }
+`;
+export const IconsNav = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2em;
+  cursor: pointer;
 `;
---
+++ src/styled/theme.tsx
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: '#799283' as const,
+  white: '#ffffff',
+  whiteThree: '#EBF1EF',
+  whiteTwo: '#BEBEBE',
+  black: '#000',
+};
