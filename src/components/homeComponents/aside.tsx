57a552ed18bfb83159b5e4f7400bf6c7f78e925b
casandrabenpor <casandra211@gmail.com>
2023-09-04 14:40:01 +0200

    button close card sidebar

--- src/components/homeComponents/aside.tsx
+++ src/components/homeComponents/aside.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Aside } from '../../styled/homeStyled/navStyled';
-import { useTypedSelector } from '../../app/store';
+import { Aside, ButtonClose, Content } from '../../styled/homeStyled/navStyled';
+import { useTypedDispatch, useTypedSelector } from '../../app/store';
+import { toggle } from '../../features/sideBarSlice';
 
 export const AsideCart = () => {
+  const dispatch = useTypedDispatch();
+
+  const toggleSideBar = (event: any) => {
+    event.preventDefault();
+    dispatch(toggle());
+  };
   const sideBarActivated = useTypedSelector((state) => state.sidebar.activated);
 
   return sideBarActivated ? (
     <Aside>
-      <h3>Shopping Cart</h3>
+      <Content $sideBarActivated={sideBarActivated}>
+        <ButtonClose onClick={toggleSideBar}>X</ButtonClose>
+        <h3>Shopping Cart</h3>
+      </Content>
     </Aside>
   ) : (
     <></>
--- src/styled/homeStyled/navStyled.tsx
+++ src/styled/homeStyled/navStyled.tsx
@@ -43,10 +43,25 @@ export const IconsNav = styled.div`
 // ASIDE
 
 export const Aside = styled.aside`
-  padding: 4% 6% 20px;
+  box-shadow: 0px 3px 10px #00000005;
+  display: flex;
+  flex-direction: column;
+  background: ${colors.white};
+  height: 100vh;
+  width: 10%;
   border: 1px solid black;
+  padding: 2em;
+  top: 0;
+  right: 0;
+  overflow-x: hidden;
+  position: fixed;
+  padding: 1em;
+  color: red;
 `;
 export const Content = styled.div<Props>`
   width: ${(props) => (props.$sideBarActivated ? '85%' : '100%')};
   margin-right: ${(props) => (props.$sideBarActivated ? '15%' : '0%')};
+`;
+export const ButtonClose = styled.button`
+  cursor: pointer;
 `;
